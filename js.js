98c36f6c3323f5fae7b1ce4ba223e569808e2c44
Tuanxu5 <lehoangtuan012@gmail.com>
2025-08-19 12:16:06 +0700

    add domain

--- js.js
+++ js.js
@@ -16,12 +16,12 @@ var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
 if (/iPhone|iPad|iPod/.test(userAgent)) {
   openApp(
-    "cplatform://honganh.vn" + pathAndQuery.substring(1),
+    "cplatform://honganh.vn/" + pathAndQuery.substring(1),
     "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667"
   );
 } else if (/Android/.test(userAgent)) {
   openApp(
-    "intent://honganh.vn" + pathAndQuery.substring(1) + "#Intent;scheme=cplatform;package=com.frovis.seller_mobile;end",
+    "intent://honganh.vn/" + pathAndQuery.substring(1) + "#Intent;scheme=cplatform;package=com.frovis.seller_mobile;end",
     "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi"
   );
 } else {
