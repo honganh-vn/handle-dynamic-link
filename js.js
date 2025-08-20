276c463d85e77a922e66154644df117646ceb115
Tuanxu5 <lehoangtuan012@gmail.com>
2025-08-20 16:42:43 +0700

    update

--- js.js
+++ js.js
@@ -1,40 +1,32 @@
-var timeout = 500;
-var originalLink = window.location.href;
+// Biến để lưu trữ timeout
+var timeout;
 
 function handleOpenApp() {
   var pathAndQuery = window.location.pathname + window.location.search;
   var schemeUrl = "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1);
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  var fallbackUrl = /iPhone|iPad|iPod/.test(userAgent)
+    ? "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667"
+    : "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi";
 
-  // Đặt cờ đánh dấu đã cố gắng mở app
-  localStorage.setItem("hasAttemptedOpenApp", "true");
+  // Thêm sự kiện lắng nghe để hủy timeout khi người dùng rời trình duyệt
+  document.addEventListener("visibilitychange", handleVisibilityChange);
 
-  if (/iPhone|iPad|iPod/.test(userAgent)) {
-    window.location = schemeUrl;
+  // Thử mở app
+  window.location = schemeUrl;
 
-    // Chỉ chuyển hướng đến App Store nếu chưa mở được app
-    if (!localStorage.getItem("isAppOpened")) {
-      setTimeout(function () {
-        window.location.href = "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667";
-      }, 1000);
+  // Hẹn giờ để chuyển hướng đến store
+  timeout = setTimeout(function () {
+    // Nếu timeout vẫn còn, tức là app chưa mở, chuyển hướng
+    window.location.href = fallbackUrl;
+  }, 2000); // 2 giây là khoảng thời gian hợp lý
+
+  function handleVisibilityChange() {
+    // Nếu trang web bị ẩn đi, nghĩa là người dùng đã chuyển sang app khác
+    if (document.visibilityState === "hidden") {
+      clearTimeout(timeout);
+      // Xóa sự kiện lắng nghe để tránh rò rỉ bộ nhớ
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
     }
-  } else if (/Android/.test(userAgent)) {
-    window.location = schemeUrl;
-
-    if (!localStorage.getItem("isAppOpened")) {
-      setTimeout(function () {
-        window.location.href = "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi";
-      }, 1000);
-    }
-  } else {
-    window.location.href = "https://m.shop.honganh.vn/onboarding";
   }
-}
-
-// Khi app mở, bạn có thể gửi lại một tín hiệu đến trình duyệt để cập nhật localStorage
-// Ví dụ: cplatform://shop.honganh.vn?isAppOpened=true
-// Trong trang web, bạn sẽ kiểm tra query param này và cập nhật localStorage
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get("isAppOpened") === "true") {
-  localStorage.setItem("isAppOpened", "true");
 }
