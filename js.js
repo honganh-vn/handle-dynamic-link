9d5d2855e44c2f0c2c73ec87a787279b339c42c6
Tuanxu5 <lehoangtuan012@gmail.com>
2025-08-20 16:53:12 +0700

    update

--- js.js
+++ js.js
@@ -1,32 +1,38 @@
-// Biến để lưu trữ timeout
-var timeout;
+var timeout = 500;
+var originalLink = window.location.href;
+
+function openApp(url, fallbackUrl) {
+  var iframe = document.createElement("iframe");
+  iframe.style.display = "none";
+  iframe.src = url;
+  document.body.appendChild(iframe);
+
+  setTimeout(function () {
+    document.body.removeChild(iframe);
+
+    window.location.href = fallbackUrl;
+  }, timeout);
+
+  setTimeout(function () {
+    window.location.href = originalLink;
+  }, timeout + 500);
+}
 
 function handleOpenApp() {
   var pathAndQuery = window.location.pathname + window.location.search;
-  var schemeUrl = "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1);
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  var fallbackUrl = /iPhone|iPad|iPod/.test(userAgent)
-    ? "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667"
-    : "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi";
 
-  // Thêm sự kiện lắng nghe để hủy timeout khi người dùng rời trình duyệt
-  document.addEventListener("visibilitychange", handleVisibilityChange);
-
-  // Thử mở app
-  window.location = schemeUrl;
-
-  // Hẹn giờ để chuyển hướng đến store
-  timeout = setTimeout(function () {
-    // Nếu timeout vẫn còn, tức là app chưa mở, chuyển hướng
-    window.location.href = fallbackUrl;
-  }, 2000); // 2 giây là khoảng thời gian hợp lý
-
-  function handleVisibilityChange() {
-    // Nếu trang web bị ẩn đi, nghĩa là người dùng đã chuyển sang app khác
-    if (document.visibilityState === "hidden") {
-      clearTimeout(timeout);
-      // Xóa sự kiện lắng nghe để tránh rò rỉ bộ nhớ
-      document.removeEventListener("visibilitychange", handleVisibilityChange);
-    }
+  if (/iPhone|iPad|iPod/.test(userAgent)) {
+    openApp(
+      "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1),
+      "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667"
+    );
+  } else if (/Android/.test(userAgent)) {
+    openApp(
+      "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1),
+      "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi"
+    );
+  } else {
+    window.location.href = "https://m.shop.honganh.vn/onboarding";
   }
 }
