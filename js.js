919eab788df28f8634fa5df10107694a2e81c40e
Tuanxu5 <lehoangtuan012@gmail.com>
2025-08-20 09:50:32 +0700

    update]

--- js.js
+++ js.js
@@ -1,1 +1,38 @@
+var timeout = 500;
 
+function openApp(url, fallbackUrl) {
+  var iframe = document.createElement("iframe");
+  iframe.style.display = "none";
+  iframe.src = url;
+  document.body.appendChild(iframe);
+
+  setTimeout(function () {
+    document.body.removeChild(iframe);
+
+    // Nếu app không mở -> fallback
+    window.location.href = fallbackUrl;
+  }, timeout);
+
+  setTimeout(function () {
+    window.location.href = "http://dlink.honganh.vn/success.html";
+  }, timeout + 500);
+}
+
+function handleOpenApp() {
+  var pathAndQuery = window.location.pathname + window.location.search;
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/iPhone|iPad|iPod/.test(userAgent)) {
+    openApp(
+      "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1),
+      "https://apps.apple.com/us/app/%C4%91%E1%BA%A1i-l%C3%BD-h%E1%BB%93ng-anh/id6470966667"
+    );
+  } else if (/Android/.test(userAgent)) {
+    openApp(
+      "cplatform://shop.honganh.vn/" + pathAndQuery.substring(1),
+      "https://play.google.com/store/apps/details?id=com.honganhprod.sellermobile&hl=vi"
+    );
+  } else {
+    window.location.href = "https://m.shop.honganh.vn/onboarding";
+  }
+}
